b3dd16ad767b0fc1631f109a8a551d4061f18c75
Youness Abarrou <abarrou.youness@gmail.com>
2021-02-06 13:39:08 +0100

    Add files via upload

---
+++ thirdpartyservices.js
@@ -0,0 +1,252 @@
+
+ 
+var thirdPartyServicesJsonArray = [
+  {
+    "name": "Google Play Services",
+    "model": "gps",
+    "gps": true,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/gps.png",
+    "link": {
+      "privacy": "https://www.google.com/policies/privacy/",
+      "terms": "https://policies.google.com/terms"
+    }
+  },
+  {
+    "name": "AdMob",
+    "model": "admob",
+    "admob": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/admob.png",
+    "link": {
+      "privacy": "https://support.google.com/admob/answer/6128543?hl=en",
+      "terms": "https://developers.google.com/admob/terms"
+    }
+  },
+  {
+    "name": "Google Analytics for Firebase",
+    "model": "firebase",
+    "firebase": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/firebase.png",
+    "link": {
+      "privacy": "https://firebase.google.com/policies/analytics",
+      "terms": "https://firebase.google.com/terms/analytics"
+    }
+  },
+  {
+    "name": "Firebase Crashlytics",
+    "model": "crashlytics",
+    "crashlytics": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/firebase.png",
+    "link": {
+      "privacy": "https://firebase.google.com/support/privacy/",
+      "terms": "https://firebase.google.com/terms/crashlytics"
+    }
+  },
+  {
+    "name": "Facebook",
+    "model": "facebook",
+    "facebook": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/facebook.png",
+    "link": {
+      "privacy": "https://www.facebook.com/about/privacy/update/printable",
+      "terms": "https://www.facebook.com/legal/terms/plain_text_terms"
+    }
+  },
+  {
+    "name": "Fabric",
+    "model": "fabric",
+    "fabric": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/fabric.png",
+    "link": {
+      "privacy": "https://policies.google.com/privacy",
+      "terms": "https://fabric.io/terms/fabric"
+    }
+  },
+  {
+    "name": "Matomo",
+    "model": "piwik",
+    "piwik": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/piwik.png",
+    "link": {
+      "privacy": "https://matomo.org/privacy-policy/",
+      "terms": "https://matomo.org/terms"
+    }
+  },
+  {
+    "name": "Clicky",
+    "model": "clicky",
+    "clicky": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/clicky.png",
+    "link": {
+      "privacy": "https://clicky.com/terms#privacy",
+      "terms": "https://clicky.com/terms/"
+    }
+  },
+  {
+    "name": "Flurry Analytics",
+    "model": "flurry",
+    "flurry": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/flurry.png",
+    "link": {
+      "privacy": "https://privacy.oath.com",
+      "terms": "https://developer.yahoo.com/flurry/legal-privacy/terms-service/flurry-analytics-terms-service.html"
+    }
+  },
+  {
+    "name": "Appodeal",
+    "model": "appodeal",
+    "appodeal": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/appodeal.png",
+    "link": {
+      "privacy": "https://www.appodeal.com/home/privacy-policy/",
+      "terms": "https://www.appodeal.com/home/terms-of-service/"
+    }
+  },
+  {
+    "name": "Fathom Analytics",
+    "model": "fathom",
+    "fathom": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/fathom.png",
+    "link": {
+      "privacy": "https://usefathom.com/privacy/",
+      "terms": "https://usefathom.com/terms/"
+    }
+  },
+  {
+    "name": "Unity",
+    "model": "unity",
+    "unity": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/unity.png",
+    "link": {
+      "privacy": "https://unity3d.com/legal/privacy-policy",
+      "terms": "https://unity3d.com/legal/terms-of-service"
+    }
+  },
+  {
+    "name": "SDKBOX",
+    "model": "sdkbox",
+    "sdkbox": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/sdkbox.png",
+    "link": {
+      "privacy": "https://www.sdkbox.com/privacy",
+      "terms": "https://www.sdkbox.com/privacy"
+    }
+  },
+  {
+    "name": "GameAnalytics",
+    "model": "gameanalytics",
+    "gameanalytics": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/gameanalytics.png",
+    "link": {
+      "privacy": "https://gameanalytics.com/privacy",
+      "terms": "https://gameanalytics.com/terms"
+    }
+  },
+  {
+    "name": "One Signal",
+    "model": "onesignal",
+    "onesignal": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/one_signal.svg",
+    "link": {
+      "privacy": "https://onesignal.com/privacy_policy",
+      "terms": "https://onesignal.com/tos"
+    }
+  },
+  {
+    "name": "Expo",
+    "model": "expo",
+    "expo": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/expo.png",
+    "link": {
+      "privacy": "https://expo.io/privacy",
+      "terms": "https://expo.io/terms"
+    }
+  },
+  {
+    "name": "Sentry",
+    "model": "sentry",
+    "sentry": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/sentry.png",
+    "link": {
+      "privacy": "https://sentry.io/privacy/",
+      "terms": "https://sentry.io/terms/"
+    }
+  },
+  {
+    "name": "AppLovin",
+    "model": "applovin",
+    "applovin": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/applovin.png",
+    "link": {
+      "privacy": "https://www.applovin.com/privacy/",
+      "terms": "https://www.applovin.com/terms/"
+    }
+  },
+  {
+    "name": "Vungle",
+    "model": "vungle",
+    "vungle": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/vungle.png",
+    "link": {
+      "privacy": "https://vungle.com/privacy/"
+    }
+  },
+  {
+    "name": "StartApp",
+    "model": "startapp",
+    "startapp": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/startapp.png",
+    "link": {
+      "privacy": "https://www.startapp.com/privacy/",
+      "terms": "https://www.startapp.com/policy/publisher-terms/"
+    }
+  },
+  {
+    "name": "AdColony",
+    "model": "adcolony",
+    "adcolony": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/adcolony.png",
+    "link": {
+      "privacy": "https://www.adcolony.com/privacy-policy/"
+    }
+  },
+  {
+    "name": "Amplitude",
+    "model": "amplitude",
+    "amplitude": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/amplitude.svg",
+    "link": {
+      "privacy": "https://amplitude.com/privacy",
+      "terms": "https://amplitude.com/terms"
+    }
+  },
+  {
+    "name": "Adjust",
+    "model": "adjust",
+    "adjust": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/adjust.png",
+    "link": {
+      "privacy": "https://www.adjust.com/terms/privacy-policy",
+      "terms": "https://www.adjust.com/terms/general-terms-and-conditions"
+    }
+  },
+  {
+    "name": "Mapbox",
+    "model": "mapbox",
+    "mapbox": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/mapbox.png",
+    "link": {
+      "privacy": "https://www.mapbox.com/legal/privacy",
+      "terms": "https://www.mapbox.com/legal/tos"
+    }
+  },
+  {
+    "name": "Godot",
+    "model": "godot",
+    "godot": false,
+    "logo": "https://raw.githubusercontent.com/Parrou/webbloog/master/godot.png",
+    "link": {
+      "privacy": "https://godotengine.org/privacy-policy",
+      "terms": "https://godotengine.org/license"
+    }
+  }
+]
